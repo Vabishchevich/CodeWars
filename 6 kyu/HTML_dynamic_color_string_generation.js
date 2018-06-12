dea045183cb4657f70ab34100152fcf94dfb03c7
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 13:48:57 +0300

    Create HTML_dynamic_color_string_generation.js

---
+++ 6 kyu/HTML_dynamic_color_string_generation.js
@@ -0,0 +1,19 @@
+/*
+Generate a valid randomly generated rgb color string. Assume all of them have 6 digits always.
+Valid Output
+#ffffff
+#25a403
+#000001
+Non-Valid Output
+#fff
+#aaa
+#zzzzz
+cafebabe
+#a567567676576756A7
+*/
+
+// My solution
+
+var generateColor = function() {
+return "#" + Math.random().toString(16).slice(2, 8);
+};
