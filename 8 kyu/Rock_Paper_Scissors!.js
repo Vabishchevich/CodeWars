173622c6f6da3db2b585e3d3c15d0ba0e658218c
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 18:24:36 +0300

    Create Rock_Paper_Scissors!.js

---
+++ 8 kyu/Rock_Paper_Scissors!.js
@@ -0,0 +1,25 @@
+/*
+Rock Paper Scissors
+Let's play! You have to return which player won! In case of a draw return Draw!.
+Examples:
+rps('scissors','paper') // Player 1 won!
+rps('scissors','rock') // Player 2 won!
+rps('paper','paper') // Draw!
+*/
+
+// My solution
+
+const rps = (p1, p2) => {
+if ((p1 == "scissors" && p2 == "paper") || (p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock")) {
+return ("Player 1 won!");
+}
+else if ((p1 == "scissors" && p2 == "rock") || (p1 == "paper" && p2 == "scissors") || (p1 == "rock" && p2 == "paper")) {
+return ("Player 2 won!");
+}
+else if ((p1 == "rock" && p2 == "rock") || (p1 == "scissors" && p2 == "scissors") || (p1 == "paper" && p2 == "paper")) {
+return ("Draw!");
+}
+else {
+return ("Player 1 won!");
+}
+};
