a797520baf1fb35f9fbf725f6eda2c420e7f4ae9
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 18:22:57 +0300

    Create Binary_Addition.js

---
+++ 7 kyu/Binary_Addition.js
@@ -0,0 +1,12 @@
+/*
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the 
+addition.
+The binary number returned should be a string.
+*/
+
+// My solution
+
+function addBinary(a,b) {
+var form = a + b;
+return form.toString(2);
+}
