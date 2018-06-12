508b05fb4a11f8247b97a2dca5ef20b889c07369
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 14:34:21 +0300

    Update Exclamation_marks_series_#6.js

--- 8 kyu/Exclamation_marks_series_#6.js
+++ 8 kyu/Exclamation_marks_series_#6.js
@@ -19,8 +19,26 @@ Please don't post issue about difficulty or duplicate.
 
 // My solution
 
+// First option
 function remove(s, n) {
 for (var i = 0; i < n; i++) 
 s = s.replace("!", "");
+return s;
+}
+
+// Second option
+function remove(s, n) {
+while (n > 0) {
+var index = s.indexOf("!"); //indexOf() возвращает первый индекс, по которому данный 
+//элемент может быть найден в массиве или -1, если такого индекса нет.
+if (index < 0) {
+return s;
+} 
+else {
+s = s.slice(0, index) + s.slice(index + 1); //slice() возвращает новый массив, содержащий
+//копию части исходного массива.
+}
+n--;
+}
 return s;
 }
