98beb0a4e39a1f67291262e28f3e5d21d7b2c5ca
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 14:37:53 +0300

    Create Sort_the_Vowels!.js

---
+++ 7 kyu/Sort_the_Vowels!.js
@@ -0,0 +1,34 @@
+/*
+Sort the Vowels!
+In this kata, we want to sort the vowels in a special format.
+Task
+Write a function which takes a input string s and return a string in the following way:
+
+                  C|                          R|
+                  |O                          n|
+                  D|                          d|
+   "Codewars" =>  |E       "Rnd Te5T"  =>      |
+                  W|                          T|
+                  |A                          |e
+                  R|                          5|
+                  S|                          T|
+
+Notes:
+List all the Vowels on the right side of |
+List every character except Vowels on the left side of |
+Case doesn't matter
+Each line is seperated with \n
+Invalid input ( undefined / null / integer ) should return an empty string
+*/
+
+// My solution
+
+function sortVowels(s) {
+if (s == undefined || s == null || s == Number(s)) {
+return s = "";
+}
+else {
+return [...s].map(word => /[aeiou]/i.test(word) ? "|" + word : word + "|").join("\n");
+//[aeiou] - все гласные
+}
+}
