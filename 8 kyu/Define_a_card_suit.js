4d8f7b0dc52566754cc8a29226bc63f11525a446
Vabishchevich <32248488+Vabishchevich@users.noreply.github.com>
2018-06-12 15:49:06 +0300

    Create Define_a_card_suit.js

---
+++ 8 kyu/Define_a_card_suit.js
@@ -0,0 +1,175 @@
+/*
+You get any card as an argument. Your task is to return a suit of this card.
+Our deck (is preloaded):
+deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+        '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+        '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+        '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+// My solution
+
+function defineSuit(card) {
+switch (card) {
+case "2♣":
+return 'clubs';
+break;
+case "3♣":
+return 'clubs';
+break;
+case "4♣":
+return 'clubs';
+break;
+case "5♣":
+return 'clubs';
+break;
+case "6♣":
+return 'clubs';
+break;
+case "7♣":
+return 'clubs';
+break;
+case "8♣":
+return 'clubs';
+break;
+case "9♣":
+return 'clubs';
+break;
+case "10♣":
+return 'clubs';
+break;
+case "J♣":
+return 'clubs';
+break;
+case "Q♣":
+return 'clubs';
+break;
+case "K♣":
+return 'clubs';
+break;
+case "A♣":
+return 'clubs';
+break;
+case "2♠":
+return 'spades';
+break;
+case "3♠":
+return 'spades';
+break;
+case "4♠":
+return 'spades';
+break;
+case "5♠":
+return 'spades';
+break;
+case "6♠":
+return 'spades';
+break;
+case "7♠":
+return 'spades';
+break;
+case "8♠":
+return 'spades';
+break;
+case "9♠":
+return 'spades';
+break;
+case "10♠":
+return 'spades';
+break;
+case "J♠":
+return 'spades';
+break;
+case "Q♠":
+return 'spades';
+break;
+case "K♠":
+return 'spades';
+break;
+case "A♠":
+return 'spades';
+break; 
+case "2♦":
+return 'diamonds';
+break;
+case "3♦":
+return 'diamonds';
+break;
+case "4♦":
+return 'diamonds';
+break;
+case "5♦":
+return 'diamonds';
+break;
+case "6♦":
+return 'diamonds';
+break;
+case "7♦":
+return 'diamonds';
+break;
+case "8♦":
+return 'diamonds';
+break;
+case "9♦":
+return 'diamonds';
+break;
+case "10♦":
+return 'diamonds';
+break;
+case "J♦":
+return 'diamonds';
+break;
+case "Q♦":
+return 'diamonds';
+break;
+case "K♦":
+return 'diamonds';
+break;
+case "A♦":
+return 'diamonds';
+break;
+case "2♥":
+return 'hearts';
+break;
+case "3♥":
+return 'hearts';
+break;
+case "4♥":
+return 'hearts';
+break;
+case "5♥":
+return 'hearts';
+break;
+case "6♥":
+return 'hearts';
+break;
+case "7♥":
+return 'hearts';
+break;
+case "8♥":
+return 'hearts';
+break;
+case "9♥":
+return 'hearts';
+break;
+case "10♥":
+return 'hearts';
+break;
+case "J♥":
+return 'hearts';
+break;
+case "Q♥":
+return 'hearts';
+break;
+case "K♥":
+return 'hearts';
+break;
+case "A♥":
+return 'hearts';
+break;
+}
+}
